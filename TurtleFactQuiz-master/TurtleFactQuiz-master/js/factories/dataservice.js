4b0c647da6cb537c27faba2396caca5f847c6e86
tahoanghainam <tahoanghainam@gmail.com>
2017-05-19 12:13:10 +0630

    upload file

---
+++ TurtleFactQuiz-master/TurtleFactQuiz-master/js/factories/dataservice.js
@@ -0,0 +1,160 @@
+/*
+ * IIFE to keep code safe and outside the global namespace
+ */
+(function(){
+
+    /*
+     * Declaring a factory service as part of the existing turtleFacts Module.
+     */
+    angular
+        .module("turtleFacts")
+        .factory("DataService", DataService);
+
+    /*
+     * Actual definition of the function used for this factory
+     */
+    function DataService(){
+        /*
+         * dataObj is used to simulate getting the data from a backend server
+         * The object will hold data which will then be returned to the other
+         * factory declared in js/factory/quiz.js which has this factory
+         * as a dependency
+         */
+
+        var dataObj = {
+            turtlesData: turtlesData,
+            quizQuestions: quizQuestions,
+            correctAnswers: correctAnswers
+        };
+
+        // returning the dataObj to anything that uses this factory as a 
+        // dependency
+        return dataObj;
+    }
+
+    /*
+     * all of the below variables are simulating data that would typically be 
+     * retrieved using an HTTP call to an API endpoint.
+     *
+     * For simplicity sake this data is hardcoded into the app as this tutorial
+     * is about building the angular app, not the backend.
+     *
+     * The correctAnswers variable would be retrieved when the user has 
+     * finished the quiz and would be used to mark the users answers against 
+     * the correct answers
+     *
+     * the quizQuestions is an array of objects, each containing data 
+     * pertaining to a single question. This includes:
+     *                          - The type of question: image or text
+     *                          - Text of the question (aka the actual question)
+     *                          - A set of 4 possible answers, either text or 
+     *                              images as indicated above
+     *                          - a selected flag which will be used to know if
+     *                              the user has selected 
+     *                          an answer yet.
+     *                          - Whether the user got the question correct or 
+     *                              not
+     *
+     * The final turtleData variable hold the information that will be 
+     * displayed in the list view when the app loads. This includes the name 
+     * and an image of each turtle as well as other information such as the 
+     * location and the size of the turtles
+     *
+     */
+
+    var correctAnswers = [1, 2, 3];
+
+    var quizQuestions  = [
+        {
+            type: "text",
+            text: "How much can a loggerhead weigh?",
+            possibilities: [
+                {
+                    answer: "Up to 20kg"
+                },
+                {
+                    answer: "Up to 115kg"
+                },
+                {
+                    answer: "Up to 220kg"
+                },
+                {
+                    answer: "Up to 500kg"
+                }
+            ],
+            selected: null,
+            correct: null
+        },
+        {
+            type: "text",
+            text: "What is the typical lifespan of a Green Sea Turtle?",
+            possibilities: [
+                {
+                    answer: "150 years"
+                },
+                {
+                    answer: "10 years"
+                },
+                {
+                    answer: "80 years"
+                },
+                {
+                    answer: "40 years"
+                }
+            ],
+            selected: null,
+            correct: null
+        },
+        {
+            type: "image",
+            text: "Which of these is the Alligator Snapping Turtle?",
+            possibilities: [
+                {
+                    answer: "https://c1.staticflickr.com/3/2182/2399413165_bcc8031cac_z.jpg?zz=1"
+                },
+                {
+                    answer: "http://images.nationalgeographic.com/wpf/media-live/photos/000/006/cache/ridley-sea-turtle_688_600x450.jpg"
+                },
+                {
+                    answer: "https://static-secure.guim.co.uk/sys-images/Guardian/Pix/pictures/2011/8/13/1313246505515/Leatherback-turtle-007.jpg"
+                },
+                {
+                    answer: "https://upload.wikimedia.org/wikipedia/commons/e/e3/Alligator_snapping_turtle_-_Geierschildkr%C3%B6te_-_Alligatorschildkr%C3%B6te_-_Macrochelys_temminckii_01.jpg"
+                }
+            ],
+            selected: null,
+            correct: null
+        }
+    ];
+
+    var turtlesData = [
+        {
+            type: "Green Turtle",
+            image_url: "http://www.what-do-turtles-eat.com/wp-content/uploads/2014/10/Sea-Turtles-Habitat.jpg",
+            locations: "Tropical and subtropical oceans worldwide",
+            size: "Up to 1.5m and up to 300kg",
+            lifespan: "Over 80 years",
+            diet: "Herbivore",
+            description: "The green turtle is a large, weighty sea turtle with a wide, smooth carapace, or shell. It inhabits tropical and subtropical coastal waters around the world and has been observed clambering onto land to sunbathe. It is named not for the color of its shell, which is normally brown or olive depending on its habitat, but for the greenish color of its skin. There are two types of green turtles—scientists are currently debating whether they are subspecies or separate species—including the Atlantic green turtle, normally found off the shores of Europe and North America, and the Eastern Pacific green turtle, which has been found in coastal waters from Alaska to Chile."
+        },
+        {
+            type: "Loggerhead Turtle",
+            image_url: "http://i.telegraph.co.uk/multimedia/archive/02651/loggerheadTurtle_2651448b.jpg",
+            locations: "Tropical and subtropical oceans worldwide",
+            size: "90cm, 115kg",
+            lifespan: "More than 50 years",
+            diet: "Carnivore",
+            description: "Loggerhead turtles are the most abundant of all the marine turtle species in U.S. waters. But persistent population declines due to pollution, shrimp trawling, and development in their nesting areas, among other factors, have kept this wide-ranging seagoer on the threatened species list since 1978. Their enormous range encompasses all but the most frigid waters of the world's oceans. They seem to prefer coastal habitats, but often frequent inland water bodies and will travel hundreds of miles out to sea."
+        },
+        {
+            type: "Leatherback Turtle",
+            image_url: "https://static-secure.guim.co.uk/sys-images/Guardian/Pix/pictures/2011/8/13/1313246505515/Leatherback-turtle-007.jpg",
+            locations: "All tropical and subtropical oceans",
+            size: "Up to 2m, up to 900kg",
+            lifespan: "45 years",
+            diet: "Carnivore",
+            description: "Leatherbacks are the largest turtles on Earth, growing up to seven feet (two meters) long and exceeding 2,000 pounds (900 kilograms). These reptilian relics are the only remaining representatives of a family of turtles that traces its evolutionary roots back more than 100 million years. Once prevalent in every ocean except the Arctic and Antarctic, the leatherback population is rapidly declining in many parts of the world. While all other sea turtles have hard, bony shells, the inky-blue carapace of the leatherback is somewhat flexible and almost rubbery to the touch. Ridges along the carapace help give it a more hydrodynamic structure. Leatherbacks can dive to depths of 4,200 feet (1,280 meters)—deeper than any other turtle—and can stay down for up to 85 minutes."
+        }
+    ];
+
+})();
